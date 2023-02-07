37a490aedc0bbe70faae534dcd2621cfb9508a3a
Jose <jose.mar.123@hotmail.com>
2023-02-06 22:28:34 -0600

    Add JS function to create card view
    Added a js function to create a card view of the product. Added
    card styles, removed HTML card template so it could be created
    and inserted by the function instead.

--- script.js
+++ script.js
@@ -31,6 +31,26 @@ const myFunc = () => {
   const myInfo = convertJSONintoFormatedString(myJSON);
   const jsonDisplayArea = document.getElementById("json-display-area");
   jsonDisplayArea.innerHTML = myInfo;
+  createCardView(myJSON);
 };
 
-const createCardView = () => {};
+const createCardView = (json) => {
+  const cardHTML = `
+    <div class="card  text-center border-0" style="width: 18rem">
+      <img src="${json.imgPath}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${json.name}</h5>
+        <a
+          href="#"
+          class="btn btn-primary btn-lg border-0 btn-rounded"
+          id="add-btn"
+        >
+          AÑADIR
+        </a>
+      </div>
+    </div>
+ `;
+
+  const cardDisplayArea = document.getElementById("card-view");
+  cardDisplayArea.innerHTML = cardHTML;
+};
